558768223cf6a71affcdccd928ccbf1f564dea7a
Andy Pickler <andy.pickler@gmail.com>
2018-06-12 12:51:03 -0600

    feat(test-app): Integrate functional test app

---
+++ src/app/customers/containers/customers/customers.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { LoadMany } from 'ngrx-auto-entity';
+import { Observable } from 'rxjs';
+
+import { Customer } from '@state/customer/customer.model';
+import { selectAllCustomers } from '@state/customer/customer.reducer';
+
+interface IAppState {
+  customer: Customer;
+}
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers$: Observable<Customer[]>;
+
+  constructor(private store: Store<IAppState>) {}
+
+  ngOnInit() {
+    this.store.dispatch(new LoadMany(Customer));
+    this.customers$ = this.store.pipe(select(selectAllCustomers));
+  }
+}
---
+++ src/app/state/customer/customer.model.ts
@@ -0,0 +1,4 @@
+export class Customer {
+  id: number;
+  name: string;
+}
