55072a6dfbd300955476f64c0dd893838c099e76
paquettepy <paquettepy@gmail.com>
2019-11-02 12:41:52 -0400

    Some cleanup

--- src/app/+orders/orders.routing.ts
+++ src/app/+orders/orders.routing.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { OrdersComponent } from 'src/app/+orders/containers/orders/orders.component';
-import { OrderComponent } from './components/order/order.component';
 
 export const ordersRoutes: Routes = [
   {
     path: '',
     component: OrdersComponent
-  },
-  {
-    path: ':id',
-    component: OrderComponent
   }
 ];
