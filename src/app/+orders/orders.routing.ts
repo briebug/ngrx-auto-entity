19da5c5a6e3213bc7c20478f8f7fb0ebd414bc45
paquettepy <paquettepy@gmail.com>
2019-11-04 12:07:26 -0500

    Uncommented orders routing (commented for testing earlier)

--- src/app/+orders/orders.routing.ts
+++ src/app/+orders/orders.routing.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { OrdersComponent } from 'src/app/+orders/containers/orders/orders.component';
 
 export const ordersRoutes: Routes = [
-  // {
-  //   path: '',
-  //   component: OrdersComponent
-  // }
+  {
+    path: '',
+    component: OrdersComponent
+  }
 ];
