03116874e95bbf49d8ef1bf6cfc0e0a5c27001cb
Kevin Schuchard <schuchard.kevin@gmail.com>
2020-09-13 12:04:08 -0500

    chore: remove other test apps

--- projects/test-app/src/app/state/account.state.ts
+++
@@ -1,9 +0,0 @@
-import { buildState, IEntityState } from '@briebug/ngrx-auto-entity';
-import { Action } from '@ngrx/store';
-import { Account } from '../models/account.model';
-
-export const { initialState, facade: AccountFacadeBase } = buildState(Account);
-
-export function accountReducer(state = initialState, action: Action): IEntityState<Account> {
-  return state;
-}
