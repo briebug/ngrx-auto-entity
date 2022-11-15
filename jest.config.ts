a611cac65a4e02629dd33f63ec5d65a3254c756c
Cole Sanders <cole.sanders@briebug.com>
2022-11-15 10:46:57 -0700

    chore: ngrx auto entity service
      * Swapped karma config for jest
      * Updated tsconfigs to match main lib and fix unit tests
      + Added ngrx-auto-entity-service to root jest project list

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 module.exports = {
   projects: [
-    '<rootDir>/projects/ngrx-auto-entity/jest.config.ts'
+    '<rootDir>/projects/ngrx-auto-entity/jest.config.ts',
+    '<rootDir>/projects/ngrx-auto-entity-service/jest.config.ts',
   ]
 };
