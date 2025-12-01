714e0f347b6a921ec8f50f5803a6a48b40dae7e8
Kevin Schuchard <schuchard.kevin@gmail.com>
2025-12-01 14:20:30 -0600

    chore(ci): simplify test command in GitHub Actions workflow
    - Remove JEST_JUNIT_OUTPUT environment variable
    - Change test command from test:ci to test

--- projects/ngrx-auto-entity-service/jest.config.ts
+++ projects/ngrx-auto-entity-service/jest.config.ts
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/../../setup-jest.ts', 'jest-extended/all'],
   globalSetup: 'jest-preset-angular/global-setup',
-  testPathIgnorePatterns: ['src/test.ts']
+  testPathIgnorePatterns: ['src/test.ts'],
+  moduleNameMapper: {
+    '^@briebug/ngrx-auto-entity$': '<rootDir>/../ngrx-auto-entity/src/public_api.ts'
+  }
 };
