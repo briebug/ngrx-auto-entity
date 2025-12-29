8b06902aeaf916a2fe1f588aca41da138ff41a1c
Stratton Whisler <stratton.whisler@briebug.com>
2025-12-29 14:44:55 -0800

    fix: adds deps to NgrxAutoEntityServiceModule.forRoot

--- projects/ngrx-auto-entity-service/src/lib/ngrx-auto-entity-service.module.ts
+++ projects/ngrx-auto-entity-service/src/lib/ngrx-auto-entity-service.module.ts
@@ -8,10 +8,13 @@ export class NgrxAutoEntityServiceModule {
   static forRoot(config: () => AutoEntityServiceConfig): ModuleWithProviders<NgrxAutoEntityServiceModule>;
   /** @deprecated use `inject` to provide dependencies */
   static forRoot(config: (...deps: any[]) => AutoEntityServiceConfig, deps: any[]): ModuleWithProviders<NgrxAutoEntityServiceModule>;
-  static forRoot(config: AutoEntityServiceConfig | (() => AutoEntityServiceConfig)): ModuleWithProviders<NgrxAutoEntityServiceModule> {
+  static forRoot(
+    config: AutoEntityServiceConfig | (() => AutoEntityServiceConfig),
+    deps?: any[]
+  ): ModuleWithProviders<NgrxAutoEntityServiceModule> {
     return {
       ngModule: NgrxAutoEntityServiceModule,
-      providers: _provideAutoEntityService(config)
+      providers: _provideAutoEntityService(config, deps)
     };
   }
 }
